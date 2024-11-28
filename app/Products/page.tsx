39717e696fe1abaa91bbfe49e209ca87a7f8e607
1MuhireDavid <muhiredavid74@gmail.com>
2024-11-28 23:06:22 +0200

    ft: implement productlist design

--- app/Products/page.tsx
+++ app/Products/page.tsx
@@ -1,12 +1,17 @@
 import React from 'react'
-import Header from '../components/Header'
-import Footer from '../components/Footer'
+import Header from '../../components/Header'
+import Footer from '../../components/Footer'
+import Product from '@/components/ProductList'
+import Bestdeal from '@/components/Bestdeal'
+import FilterBar from '@/components/FilterBar'
 
 const page = () => {
   return (
     <div>
       <Header/>
-      
+      <FilterBar />
+      <Product/>
+      <Bestdeal/>
       <Footer/>
     </div>
   )

600d7a5b29d2efdc2b1e7ce89e9187ef2b9e57f7
kagisaac <kagisaac103@gmail.com>
2025-04-29 13:39:17 +0300

    fixing the build error for image naming

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -219,7 +219,7 @@ export default function AboutPage() {
                 },
                 {
                   name: 'Uwanyagasani Eric',
-                  position: 'Graphic Designer',
+                  position: 'UI/UX & Graphic Designer',
                   image: kappy,
                   linkedin: 'https://www.linkedin.com/in/eric-uwanyagasani-936082230/',
                 },
